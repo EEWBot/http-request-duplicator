6b5ea0cf6386a4d30875f3fe374c641978170e23
yanorei32 <yanorei@hotmail.co.jp>
2024-01-24 10:22:34 +0900

    Apply formatter

--- src/duplicator/queue/mod.rs
+++ src/duplicator/queue/mod.rs
@@ -1,4 +1,6 @@
 mod queue;
 mod two_level_queue;
 
-pub use two_level_queue::{two_level_queue, TwoLevelQueueSender, TwoLevelQueueReceiver, Priorized, Priority};
+pub use two_level_queue::{
+    two_level_queue, Priority, Priorized, TwoLevelQueueReceiver, TwoLevelQueueSender,
+};
